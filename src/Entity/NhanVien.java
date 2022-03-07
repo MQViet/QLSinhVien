1627876cae63387dc95c793f4acc11c9ef77df25
vietmqps15755 <97033463+vietmqps15755@users.noreply.github.com>
2022-03-08 01:17:56 +0700

    commit QLSV

---
+++ src/Entity/NhanVien.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entity;
+
+/**
+ *
+ * @author vietm
+ */
+public class NhanVien {
+
+    private String MaNV;
+    private String MatKhau;
+    private String HoTen;
+    private boolean VaiTro;
+    private String GioiTinh;
+
+    public String getMaNV() {
+        return MaNV;
+    }
+
+    public void setMaNV(String MaNV) {
+        this.MaNV = MaNV;
+    }
+
+    public String getMatKhau() {
+        return MatKhau;
+    }
+
+    public void setMatKhau(String MatKhau) {
+        this.MatKhau = MatKhau;
+    }
+
+    public String getHoTen() {
+        return HoTen;
+    }
+
+    public void setHoTen(String HoTen) {
+        this.HoTen = HoTen;
+    }
+
+    public boolean isVaiTro() {
+        return VaiTro;
+    }
+
+    public void setVaiTro(boolean VaiTro) {
+        this.VaiTro = VaiTro;
+    }
+
+    public String getGioiTinh() {
+        return GioiTinh;
+    }
+
+    public void setGioiTinh(String GioiTinh) {
+        this.GioiTinh = GioiTinh;
+    }
+    
+            
+}
